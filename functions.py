90a32a04562b75fea261a56ecc9547a44e6add10
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-06 21:32:30 +0200

    Add descriptions to functions

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
 def get_todos(filepath="todos.txt"):
+    """This function read todos from a file and return them."""
     with open(filepath, "r") as file_local:
         local_todos = file_local.readlines()
     return local_todos
 
 def write_todos(local_todos, filepath="todos.txt"):
+    """Write the todos to a file with extention .txt"""
     with open(filepath, "w") as file_local:
         file_local.writelines(local_todos)
