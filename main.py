06e1cf53edd3dae82d25ee616f1db85590a48bb2
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-06 20:55:47 +0200

    Add option for edit and complete the tasks

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ while True:
     elif user_choice == "Show":
         for index, task in enumerate(todos):
             print(f"{index+1}: {task}")
+    elif user_choice == "Edit":
+        number = int(input("Which task do you wanna edit: "))
+        new_task = input("Enter the edit task: ")
+        todos[number-1] = new_task.capitalize()
+    elif user_choice == "Complete":
+        number = int(input("Which task do you wanna complete: "))
+        print(f"The task {todos[number-1]} was completed!")
+        todos.pop(number-1)
     elif user_choice == "Exit":
         print("Bye")
         break
